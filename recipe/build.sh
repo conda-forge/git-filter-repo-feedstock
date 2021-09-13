0a6c75abcc7853c50edaa7a8795a8ccb41dba7ae
Duncan Macleod <duncan.macleod@ligo.org>
2021-09-13 10:25:18 +0100

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,6 @@ set -e
 ${PYTHON} -m pip install . -vv
 
 # install manpage
-_MANDIR="${PREFIX}/man/man1"
+_MANDIR="${PREFIX}/share/man/man1"
 install -d -m 0755 ${_MANDIR}
 install -m 0644 orig/Documentation/man1/git-filter-repo.1 ${_MANDIR}/git-filter-repo.1
